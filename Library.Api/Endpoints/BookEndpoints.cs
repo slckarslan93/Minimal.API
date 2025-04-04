8a58b4edd7942264bf85a3933b6024c6e1eac0dc
Selcuk <slckarslan93@gmail.com>
2025-04-05 01:25:55 +0300

    Refactor book endpoints into separate class
    Moved endpoint definitions from `Program.cs` to a new static class `BookEndpoints` in `BookEndpoints.cs`. This change enhances code organization by separating endpoint logic from application startup logic. The `UseBookEndpoints` method encapsulates all book-related endpoints, which are now registered in `Program.cs`. The functionality of the `login` endpoint and CRUD operations for books remains intact, improving code readability and maintainability.

---
+++ Library.Api/Endpoints/BookEndpoints.cs
@@ -0,0 +1,80 @@
+﻿using FluentValidation.Results;
+using Library.Api.Models;
+using Library.Api.Services;
+using Library.Api.Validator;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Library.Api.Endpoints;
+
+public static class BookEndpoints
+{
+    public static void UseBookEndpoints(this IEndpointRouteBuilder app)
+    {
+        app.MapGet("login", (JwtProvider jwtProvider) =>
+        {
+            return Results.Ok(new { Token = jwtProvider.CreateToken() });
+        });
+
+        app.MapPost("books", async (Book book, IBookService bookService, CancellationToken cancellationToken) =>
+        {
+            BookValidator validator = new();
+            ValidationResult validationResult = validator.Validate(book);
+
+            if (!validationResult.IsValid)
+            {
+                return Results.BadRequest(validationResult.Errors.Select(s => s.ErrorMessage));
+            }
+
+            var result = await bookService.CreateAsync(book, cancellationToken);
+            if (!result) return Results.BadRequest("Someting went wrong");
+
+            return Results.Created("GetBook", new { isbn = book.Isbn });
+            //return Results.Created($"/books/{book.Isbn}",book);
+        });
+
+
+        app.MapGet("books", [Authorize] async (IBookService bookService, CancellationToken cancellationToken) =>
+        {
+            var books = await bookService.GetAllAsync(cancellationToken);
+            return Results.Ok(books);
+        });
+
+        app.MapGet("books/{isbn}", async (string isbn, IBookService bookService, CancellationToken cancellationToken) =>
+        {
+            Book? book = await bookService.GetByIsbnAsync(isbn, cancellationToken);
+            return Results.Ok(book);
+        }).WithName("GetBook");
+
+        app.MapGet("getBooksByTitle/{title}", async (string title, IBookService bookService, CancellationToken cancellationToken) =>
+        {
+            var books = await bookService.SearchByTitleAsync(title, cancellationToken);
+            return Results.Ok(books);
+        });
+
+        app.MapPut("books", async (Book book, IBookService bookService, CancellationToken cancellationToken) =>
+        {
+            BookValidator validator = new();
+            ValidationResult validationResult = validator.Validate(book);
+
+            if (!validationResult.IsValid)
+            {
+                return Results.BadRequest(validationResult.Errors.Select(s => s.ErrorMessage));
+            }
+
+            var result = await bookService.UpdateAsync(book, cancellationToken);
+            if (!result) return Results.BadRequest("Someting went wrong");
+
+            return Results.Ok(new { Message = "Book update is successfull" });
+        });
+
+        app.MapDelete("books/{isbn}", async (string isbn, IBookService bookService, CancellationToken cancellationToken) =>
+        {
+            var result = await bookService.DeleteAsync(isbn, cancellationToken);
+
+            if (!result) return Results.BadRequest("Someting went wrong");
+
+            return Results.Ok(new { Message = "Book delete is successfull" });
+        });
+
+    }
+}
--- Library.Api/Program.cs
+++ Library.Api/Program.cs
@@ -1,6 +1,7 @@
 using System.Threading;
 using FluentValidation.Results;
 using Library.Api.Context;
+using Library.Api.Endpoints;
 using Library.Api.Models;
 using Library.Api.Services;
 using Library.Api.Validator;
@@ -42,72 +43,7 @@ var app = builder.Build();
 
 app.UseSwagger();
 app.UseSwaggerUI();
-app.MapGet("login", (JwtProvider jwtProvider) =>
-{
-    return Results.Ok(new { Token = jwtProvider.CreateToken() }); 
-});
 
-app.MapPost("books", async (Book book, IBookService bookService ,CancellationToken cancellationToken) =>
-{
-    BookValidator validator = new();
-    ValidationResult validationResult =  validator.Validate(book);
-
-    if (!validationResult.IsValid)
-    {
-        return Results.BadRequest(validationResult.Errors.Select(s => s.ErrorMessage));
-    }
-
-    var result = await bookService.CreateAsync(book, cancellationToken);
-    if (!result) return Results.BadRequest("Someting went wrong");
-
-    return Results.Created("GetBook", new { isbn = book.Isbn});
-    //return Results.Created($"/books/{book.Isbn}",book);
-});
-
-
-app.MapGet("books",[Authorize] async (IBookService bookService , CancellationToken cancellationToken) =>
-{
-    var books = await bookService.GetAllAsync(cancellationToken);
-    return Results.Ok(books);
-});
-
-app.MapGet("books/{isbn}", async (string isbn, IBookService bookService, CancellationToken cancellationToken) =>
-{
-    Book? book = await bookService.GetByIsbnAsync(isbn, cancellationToken);
-    return Results.Ok(book);
-}).WithName("GetBook");
-
-app.MapGet("getBooksByTitle/{title}", async (string title, IBookService bookService, CancellationToken cancellationToken) =>
-{
-    var books = await bookService.SearchByTitleAsync(title, cancellationToken);
-    return Results.Ok(books);
-});
-
-app.MapPut("books", async (Book book, IBookService bookService, CancellationToken cancellationToken) =>
-{
-    BookValidator validator = new();
-    ValidationResult validationResult = validator.Validate(book);
-
-    if (!validationResult.IsValid)
-    {
-        return Results.BadRequest(validationResult.Errors.Select(s => s.ErrorMessage));
-    }
-
-    var result = await bookService.UpdateAsync(book, cancellationToken);
-    if (!result) return Results.BadRequest("Someting went wrong");
-
-    return Results.Ok(new { Message = "Book update is successfull" });
-});
-
-app.MapDelete("books/{isbn}", async (string isbn, IBookService bookService, CancellationToken cancellationToken) =>
-{
-    var result = await bookService.DeleteAsync(isbn, cancellationToken);
-
-    if(!result) return Results.BadRequest("Someting went wrong");
-
-    return Results.Ok(new { Message = "Book delete is successfull" });
-});
-
-
+app.UseBookEndpoints();
 
 app.Run();
